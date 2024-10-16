baa20876a18cb4f1533740278b261535bbf68f1c
gitWestender <javaxwestender@gmail.com>
2024-10-16 20:27:43 +0300

    initial commit

---
+++ src/test/java/com/sp/recommendation_service/RecommendationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sp.recommendation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
